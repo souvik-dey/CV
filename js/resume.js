8d2b9781fcc2cd58df693edeee873b718f51e77d
souvik-dey <dey.souvik007244@gmail.com>
2019-04-07 00:46:53 +0530

    Contents changed and style

--- js/resume.js
+++ js/resume.js
@@ -7,29 +7,29 @@ function load_about() {
 function load_experience() {
   $("#about,#experience,#education,#skills,#interests,#awards").removeClass("active");
   $("#experience").addClass("active");
-  document.getElementById("changeonclick").innerHTML = '<object type="text/html" data="experience.html" ></object>';
+  $('#changeonclick').load("experience.html");
 }
 
 function load_education() {
   $("#about,#experience,#education,#skills,#interests,#awards").removeClass("active");
   $("#education").addClass("active");
-  document.getElementById("changeonclick").innerHTML = '<object type="text/html" data="education.html" ></object>';
+  $('#changeonclick').load("education.html");
 }
 
 function load_skills() {
   $("#about,#experience,#education,#skills,#interests,#awards").removeClass("active");
   $("#skills").addClass("active");
-  document.getElementById("changeonclick").innerHTML = '<object type="text/html" data="skills.html" ></object>';
+  $('#changeonclick').load("skills.html");
 }
 
 function load_awards() {
   $("#about,#experience,#education,#skills,#interests,#awards").removeClass("active");
   $("#awards").addClass("active");
-  document.getElementById("changeonclick").innerHTML = '<object type="text/html" data="awards.html" ></object>';
+  $('#changeonclick').load("awards.html");
 }
 
 function load_interests() {
   $("#about,#experience,#education,#skills,#interests,#awards").removeClass("active");
   $("#interests").addClass("active");
-  document.getElementById("changeonclick").innerHTML = '<object type="text/html" data="interests.html" ></object>';
+  $('#changeonclick').load("interests.html");
 }
